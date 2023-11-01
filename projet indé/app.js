f338bf26ef80e0d8b232f07ac305ff2bbca613a1
yemann404 <91750838+yemann404@users.noreply.github.com>
2023-11-01 17:42:31 +0100

    Add files via upload
    projet créatif  d'un siteweb

---
+++ projet indé/app.js
@@ -0,0 +1,20 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+
+var images = document.querySelectorAll('img');
+new simpleParallax(images);
